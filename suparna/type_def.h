1b0e46671e29c1a799ba09981ac1e7e2ac496206
oliewalcp <811299838@qq.com>
2019-11-23 11:52:13 +0800

    移动预处理判断代码

--- suparna/type_def.h
+++ suparna/type_def.h
@@ -7,4 +7,34 @@
 
 using size_type = unsigned long long;
 
+
+#if __GNUC__ == 8 || __GNUC__ < 7
+#error not support current gcc version
+
+#elif __GNUC__ > 8
+
+#ifndef SUPPORT_FILESYSTEM
+#define SUPPORT_FILESYSTEM
+#endif
+
+#include <filesystem>
+namespace std_fs = std:filesystem;
+
+#elif __GNUC__ > 6
+
+#ifndef SUPPORT_FILESYSTEM
+#define SUPPORT_FILESYSTEM
+#endif
+
+#include <experimental/filesystem>
+namespace std_fs = std::experimental::filesystem;
+// 需要链接 stdc++fs库，即在编译指令添加 -lstdc++fs选项
+
+#endif
+
+#ifndef SUPPORT_FILESYSTEM
+#include <fstream>
+#endif
+
+
 #endif
